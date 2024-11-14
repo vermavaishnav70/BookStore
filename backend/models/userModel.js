b84e8faa98cbce55097c8e8b38fe6e10542a1065
Vaishnav Verma <vermavaishnav70@gmail.com>
2024-11-14 18:56:18 +0530

    add / get method

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,7 +2,7 @@ const e = require("express");
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true  },
   password: { type: String, required: true },
 });
 
